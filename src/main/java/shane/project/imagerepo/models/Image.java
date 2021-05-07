9bee7a59396236d200ac69d6b681dcd9606b37be
Shane Kaestner <shane.kaestner@yahoo.com>
2021-05-06 21:44:17 -0700

    build out the image model

--- src/main/java/shane/project/imagerepo/models/Image.java
+++ src/main/java/shane/project/imagerepo/models/Image.java
@@ -13,4 +13,46 @@ public class Image {
     @Column(nullable = false,
         unique = true)
     private String imagename;
+
+    @Column(name = "type")
+    private String type;
+
+    @Lob
+    @Column(name="pic")
+    private byte[] pic;
+
+    public Image() {
+    }
+
+    public long getImageid() {
+        return imageid;
+    }
+
+    public void setImageid(long imageid) {
+        this.imageid = imageid;
+    }
+
+    public String getImagename() {
+        return imagename;
+    }
+
+    public void setImagename(String imagename) {
+        this.imagename = imagename;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getPic() {
+        return pic;
+    }
+
+    public void setPic(byte[] pic) {
+        this.pic = pic;
+    }
 }
