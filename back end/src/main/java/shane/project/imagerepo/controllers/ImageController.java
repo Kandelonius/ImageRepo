bb258bcfbab6e867b91a3b2d4e8db59799e43220
Shane Kaestner <shane.kaestner@yahoo.com>
2021-05-07 16:47:49 -0700

    moved backend into a backend folder so I can seperate out the front end

---
+++ back end/src/main/java/shane/project/imagerepo/controllers/ImageController.java
@@ -0,0 +1,16 @@
+package shane.project.imagerepo.controllers;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+}
