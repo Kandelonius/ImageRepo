a0f339c09fd3d8c52aa7e8a3c4961ff4ba0a0cef
Shane Kaestner <shane.kaestner@yahoo.com>
2021-05-06 21:59:50 -0700

    created image repository interface and started building out the image controller.

---
+++ src/main/java/shane/project/imagerepo/controllers/ImageController.java
@@ -0,0 +1,11 @@
+package shane.project.imagerepo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+
+    
+}
---
+++ src/main/java/shane/project/imagerepo/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package shane.project.imagerepo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shane.project.imagerepo.models.Image;
+import java.util.Optional;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findByName(String name);
+}
