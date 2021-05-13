984e69454a1086b4a11cd25e0773058ded26f545
Shane Kaestner <shane.kaestner@yahoo.com>
2021-05-12 23:32:30 -0700

    added the app routing module

--- front-end/ImageRepo/src/app/app.module.ts
+++ front-end/ImageRepo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-// import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
+    AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
